f56713a03ec85362e401f28436fb487ab80f849b
Guillaume BRANCON <g.brancon@iia-laval.fr>
2020-09-09 13:22:47 +0200

    Update ViewModelViewCustomer.cs

--- MegaCasting.WPF/ViewModels/ViewModelViewCustomer.cs
+++ MegaCasting.WPF/ViewModels/ViewModelViewCustomer.cs
@@ -150,13 +150,16 @@ namespace MegaCasting.WPF.ViewModels
         /// <returns></returns>
         public static string CreateRandomPassphrase(int length)
         {
-            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-            StringBuilder res = new StringBuilder();
-            Random rnd = new Random();
-            while (0 < length--)
-            {
-                res.Append(valid[rnd.Next(valid.Length)]);
-            }
+            
+                const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                StringBuilder res = new StringBuilder();
+                Random rnd = new Random();
+                while (0 < length--)
+                {
+                    res.Append(valid[rnd.Next(valid.Length)]);
+                }
+            
+            
             return res.ToString();
         }
 
